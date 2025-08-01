45922d8900b4afd2be213b42478fb853f280b523
DevDominique <162951119+DevDominique@users.noreply.github.com>
2025-07-31 23:14:00 -0500

    implemented proper form validation with styling and rsvp count. changed rsvp backgroun to gradient and restyled about div

--- script.js
+++ script.js
@@ -21,50 +21,42 @@ const toggleDarkMode = () => {
 //             and tell it to use toggleDarkMode as its callback function
 themeButton.addEventListener("click", toggleDarkMode);
 
-/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
-let rsvpButton = document.getElementById("rsvp-button");
 
+/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
+
+//create variables for the form button and rsvp counter
+let rsvpButton = document.getElementById("rsvp-button");
+let count = 3;
+
+//write a function that adds participant to list + displays it with count
 const addParticipant = (event) => {
-    event.preventDefault();
 
     let nameInput = document.getElementById("name").value;
     let cityInput = document.getElementById("city").value;
 
     let newRSVP = document.createElement("p");
-    newRSVP.textContent = `👑 ${nameInput} from ${cityInput} signed up!`;
+    newRSVP.textContent = `📖 ${nameInput} from ${cityInput} signed up!`;
 
     let rsvpList = document.querySelector(".rsvp-participants");
     rsvpList.appendChild(newRSVP);
 
-    // const revpCount = rsvpList.querySelectorAll("p").length;
-    // const countMessage = document.createElement("p");
-    // countMessage.id = "rsvp-count";
-    // countMessage.textContent = `⭐ ${rsvpCount} people have RSVP'd to this event!`;
-    // rsvpList.appendChild(countMessage);
+    let rsvpCount = document.getElementById("rsvp-count");
+    rsvpCount.remove();
 
+    count = count + 1;
+
+    let newCount = document.createElement("p");
+    newCount.id = "rsvp-count";
+    newCount.textContent = "✨We’ve got " + count + " RSVPs — the circle is forming beautifully. Are you next?";
+
+    document.querySelector(".rsvp-participants").appendChild(newCount);
 };
 
-rsvpButton.addEventListener("click", addParticipant);
+//add click event to form submit button to add participant to list on submit
+//Deleted event listener
+// rsvpButton.addEventListener("click", addParticipant);
 
-
-const form = document.getElementById("rsvp-form");
-
-const printReceipt = (event) => {
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const message = `Thank you, ${name}! We will send you a reminder a few days before we meet!`;
-    
-
-    const confirmationDiv = document.getElementById("confirmationMessage");
-    confirmationDiv.textContent = message;
-
-};
-
-form.addEventListener("submit", printReceipt);
-
-/*** Form Validation ***
-  
+/*** Form Validation ***  
   Purpose:
   - Prevents invalid form submissions from being added to the list of participants.
 
@@ -78,21 +70,56 @@ form.addEventListener("submit", printReceipt);
 // Step 1: We actually don't need to select the form button again -- we already did it in the RSVP code above.
 
 // Step 2: Write the callback function
-const validateForm = () => {
-
+const validateForm = (event) => {
+  event.preventDefault();
+  
   let containsErrors = false;
-
   var rsvpInputs = document.getElementById("rsvp-form").elements;
-  // TODO: Loop through all inputs
 
-  // TODO: Inside loop, validate the value of each input
+  // TODO: Loop through all inputs
+  for (let i = 0; i < rsvpInputs.length; i++) {
+    let input = rsvpInputs[i];
+
+    // TODO: Inside loop, validate the value of each input
+    
+    //exclude submit button from error styling
+    if(input.type === "submit" || input.type === "button") {
+      continue;
+    }
+    
+    if(input.value.length < 2) {
+      containsErrors = true;
+      input.classList.add("error");
+    } else {
+      input.classList.remove("error");
+    }
+  }
+  //Stretch: Email Validation
+
+  //create variable for email input from form
+  let emailInput = document.getElementById("email");
+
+  //if it doesn't include both "@" AND "." for domain then flag it as an error
+  if (!emailInput.value.includes("@") || !emailInput.value.includes(".")) {
+    containsErrors = true;
+    emailInput.classList.add("error");
+  } else {
+    emailInput.classList.remove("error");
+  }
 
   // TODO: If no errors, call addParticipant() and clear fields
+  if(containsErrors === false) {
+    addParticipant(event);
 
-}
+    for (let i = 0; i < rsvpInputs.length; i++) {
+      rsvpInputs[i].value = "";
+    }
+  }
+};
 
 // Step 3: Replace the form button's event listener with a new one that calls validateForm()
-
+let form = document.getElementById("rsvp-form");
+form.addEventListener("submit", validateForm);
 /*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
 /*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
 
